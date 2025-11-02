d1c8912c98fcdc86bf041d5a3fc4049e9a952e78
lakshaybxt <bishtlakshay14@gmail.com>
2025-11-02 19:49:41 +0530

    Initialized

--- src/main/java/com/testing/ex/domain/entity/Product.java
+++ src/main/java/com/testing/ex/domain/entity/Product.java
@@ -33,7 +33,7 @@ import lombok.NoArgsConstructor;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "tenant_id", nullable = false, length = 36)
